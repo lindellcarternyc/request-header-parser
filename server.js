fbdff4451f998b2ffef3f922da0629ee68429c5c
Lindell <lindellcarternyc@gmail.com>
2017-04-30 20:26:56 +0000

    first draft of backend

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 var express = require("express");
 var app     = express();
+var uaParser = require("express-useragent");
 
 
 
 app.get('/api/whoami', (req, res) => {
-   res.send('/api/whoami'); 
+    var ipaddress = req.ip.split(':')[3];
+    
+    var language = req.acceptsLanguages()[0];
+    
+    var userAgent = uaParser.parse(req.get('User-Agent'));
+    var os = userAgent.os;
+    var platform = userAgent.platform;
+    var browser = userAgent.browser;
+    
+    var software = {
+      os,
+      platform,
+      browser
+    };
+    
+    var obj = {
+      ipaddress,
+      language,
+      software
+    };
+    
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(obj));
 });
 
 app.get('*', (req, res) => {
